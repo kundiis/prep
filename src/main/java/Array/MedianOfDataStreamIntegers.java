146cbd523ae06ab83d120bd6d07d41c79d2359cf
KM <km@KMs-MacBook-Pro.local>
2018-11-29 22:11:45 -0800

    team formation and resort

---
+++ src/main/java/Array/MedianOfDataStreamIntegers.java
@@ -0,0 +1,38 @@
+package Array;
+
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class MedianOfDataStreamIntegers {
+
+    PriorityQueue<Integer> maxHeap;//lower half
+    PriorityQueue<Integer> minHeap;//higher half
+
+    public MedianOfDataStreamIntegers(){
+        maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        minHeap = new PriorityQueue<>();
+    }
+
+    // Adds a number into the data structure.
+    public void addNum(int num) {
+
+        /* we dont know whether the number is smaller or bigger */
+        maxHeap.offer(num);
+        minHeap.offer(maxHeap.poll());
+
+        if(maxHeap.size() < minHeap.size()){
+            maxHeap.offer(minHeap.poll());
+        }
+
+    }
+
+    // Returns the median of current data stream
+    public double findMedian() {
+        if(maxHeap.size()==minHeap.size()){
+            return (double)(maxHeap.peek()+(minHeap.peek()))/2;
+        }else{
+            return maxHeap.peek();
+        }
+    }
+
+}
---
+++ src/main/java/BST/InorderSuccessorBST.java
@@ -0,0 +1,14 @@
+package BST;
+
+public class InorderSuccessorBST {
+
+    /*
+    Start with root, current = root, successor = NULL.
+    If node.value < current.value, then successor = current, current = current.left.
+    If node.value > current.value, current = current.right.
+    If node.value == current.value and node.right != null, successor = minimum(current.right).
+    Return successor
+
+     */
+
+}
